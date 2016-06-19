929de89fe594d23c83eff6b88c2e1b9a1b35a64a
Christoph Deil <Deil.Christoph@gmail.com>
2016-06-20 00:42:36 +0200

    autoformat

--- src/app/hello.component.spec.ts
+++ src/app/hello.component.spec.ts
@@ -1,22 +1,15 @@
-import {
-  beforeEachProviders,
-  describe,
-  expect,
-  it,
-  inject
-} from '@angular/core/testing';
-import { HelloAppComponent } from '../app/hello.component';
+import {beforeEachProviders, describe, expect, inject, it} from '@angular/core/testing';
+
+import {HelloAppComponent} from '../app/hello.component';
 
 beforeEachProviders(() => [HelloAppComponent]);
 
 describe('App: Hello', () => {
   it('should create the app',
-      inject([HelloAppComponent], (app: HelloAppComponent) => {
-    expect(app).toBeTruthy();
-  }));
+     inject([HelloAppComponent], (app: HelloAppComponent) => { expect(app).toBeTruthy(); }));
 
   it('should have as title \'hello works!\'',
-      inject([HelloAppComponent], (app: HelloAppComponent) => {
-    expect(app.title).toEqual('hello works!');
-  }));
+     inject([HelloAppComponent], (app: HelloAppComponent) => {
+       expect(app.title).toEqual('hello works!');
+     }));
 });
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
-import { bootstrap } from '@angular/platform-browser-dynamic';
-import { enableProdMode } from '@angular/core';
-import { HelloAppComponent, environment } from './app/';
+import {enableProdMode} from '@angular/core';
+import {bootstrap} from '@angular/platform-browser-dynamic';
+
+import {HelloAppComponent, environment} from './app/';
 
 if (environment.production) {
   enableProdMode();
